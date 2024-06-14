8218ef6354ab78d99ea5d567ecc9d894bd7b412f
Hector <hcontrerascorvacho@gmail.com>
2024-06-14 18:35:31 -0400

    form case empty fields

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function applyStyles(input) {
     var radios = document.querySelectorAll('input[name="' + input.name + '"]');
     radios.forEach(function(radio) {
@@ -10,4 +11,110 @@ function applyStyles(input) {
         container.style.backgroundColor = '#FFFFFF';
       }
     });
+
   }
+
+let nombre = document.getElementById('firstname');
+let apellido = document.getElementById('lastname');
+let mail = document.getElementById('email');
+let gral = document.getElementById('generalEnquiry');
+let support = document.getElementById('supportRequest');
+let check = document.getElementById('check');
+let submit = document.getElementById('submit');
+let msj = document.getElementById('msj');
+let p0 = document.getElementById('pf0');
+let p1 = document.getElementById('pf1');
+let p2 = document.getElementById('pf2');
+let p3 = document.getElementById('pf3'); //mail formato incorrecto
+let p4 = document.getElementById('pf4');
+let p5 = document.getElementById('pf5');
+let p6 = document.getElementById('pf6');
+let p7 = document.getElementById('pf7');
+
+submit.addEventListener('click',(e)=>{
+    e.preventDefault();
+if(nombre.value.trim()==='' ){
+    p1.style.display='block';
+    px.style.display='none';
+}
+else{
+    p1.style.display='none';
+}
+if(apellido.value.trim()==='' ){
+    p2.style.display='block';
+    p0.style.display='none';
+}
+else{
+    p2.style.display='none';
+}
+
+if(email.value.trim()==='' ){
+    p4.style.display='block';
+}
+else{
+    p4.style.display='none';
+}
+
+if (!document.querySelector('input[name="query_type"]:checked')) {
+    // Si ningún radio de tipo de consulta está seleccionado
+    p5.style.display = 'block';
+} else {
+    p5.style.display = 'none';
+}
+
+
+if (!check.checked) {
+    // Si el checkbox no está marcado
+    p7.style.display = 'block';
+} else {
+    p7.style.display = 'none';
+}
+
+if(msj.value.trim()==='' ){
+    p6.style.display='block';
+}
+else{
+    p6.style.display='none';
+}
+})
+
+
+nombre.addEventListener('blur', ()=>{
+    p1.style.display='block';
+    if(p2.style.display==''){
+        p0.style.display='block';
+    }else{
+        p0.style.display='none';
+    }
+    
+})
+
+apellido.addEventListener('blur', ()=>{
+    p2.style.display='block';
+    p0.style.display='none';
+    if(p1.style.display==''){
+        px.style.display='block';
+    }else{
+        px.style.display='none';
+    }
+})
+
+mail.addEventListener('blur', ()=>{
+    p4.style.display='block';
+})
+
+gral.addEventListener('blur', ()=>{
+    p5.style.display='block';
+})
+
+support.addEventListener('blur', ()=>{
+    p5.style.display='block';
+})
+
+msj.addEventListener('blur', ()=>{
+    p6.style.display='block';
+})
+
+check.addEventListener('blur', ()=>{
+    p7.style.display='block';
+})
